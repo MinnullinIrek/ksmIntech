4bdfcb6b6675be0709ddd119efb28356aceca9ea
irek minnullin <irekminn4@gmail.com>
2017-08-02 21:57:10 +0300

    движется цилиндр по плоскости, за собой оставляет следы из линий, камера за всем эти следит.

--- widget.cpp
+++ widget.cpp
@@ -1,16 +1,10 @@
 #include "widget.h"
 #include <QMouseEvent>
 #include <GL/glaux.h>
-#include <GL/gl.h>
 #include <math.h>
-
-
-
-
 
 Widget::Widget(QWidget *p):QGLWidget(p)
 {
-
     QObject::connect(&timer, &QTimer::timeout, this, &Widget::recalcCoords);
     timer.start(100);
 }
@@ -29,6 +23,9 @@ void Widget::recalcCoords()
     xTimer = flatSize*sin(zTimer)/2;
     alpha =  zTimer*180/3.14;
 
+    zCamera = zTimer + flatSize -  sin(zTimer) * 0.1;
+    xCamera = xTimer + cos(zTimer) * 0.1;
+    yCamera = yTimer - flatSize + 0.1;
 
     updateGL();
 }
@@ -38,49 +35,6 @@ void Widget::initializeGL()
     qglClearColor(Qt ::black);
     glEnable(GL_DEPTH_TEST);
     glShadeModel(GL_FLAT);
-    m_nFlat = createFlat();
-    /*
-    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-
-    float sp[4] = {1,1,1,1};
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT3);
-        glEnable(GL_LIGHT5);
-        glEnable(GL_LIGHT6);
-    glLightfv(GL_LIGHT3, GL_SPECULAR, sp);
-    glLightfv(GL_LIGHT5, GL_SPECULAR, sp);
-    glLightfv(GL_LIGHT6, GL_SPECULAR, sp);
-
-
-    float mat_specular[] = {1,1,1,1};
-    float pos[4] = {3,3,3,1};
-    float color[4] = {1,1,1,1};
-
-    color[1]=color[2]=0;
-     glLightfv(GL_LIGHT3, GL_DIFFUSE, color);
-     color[0]=0;
-         color[1]=1;
-     glLightfv(GL_LIGHT5, GL_DIFFUSE, color);
-     color[1]=0;
-         color[2]=1;
-     glLightfv(GL_LIGHT6, GL_DIFFUSE, color);
-     glLightfv(GL_LIGHT3, GL_POSITION, pos);
-         pos[0] = -3;
-     glLightfv(GL_LIGHT5, GL_POSITION, pos);
-         pos[0]=0;pos[1]=-3;
-     glLightfv(GL_LIGHT6, GL_POSITION, pos);
-     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-     glMaterialf(GL_FRONT, GL_SHININESS, 128.0);
-*/
-    float ambience[4] = {1.0f, 1.0f, 1.0f, 0.0};
-      float diffuse[4] = {1.0f, 1.0f, 1.0f, 1.0};
-      glLightfv(GL_LIGHT0, GL_AMBIENT,  ambience);
-      glLightfv(GL_LIGHT0, GL_DIFFUSE,  diffuse);
-
-      glEnable(GL_LIGHTING);
-      glEnable(GL_LIGHT0);
-      glEnable(GL_COLOR_MATERIAL);
 }
 
 void Widget::resizeGL(int w, int h)
@@ -101,11 +55,11 @@ void Widget::paintGL()
     glRotatef(m_xRotate, 1, 0, 0);
     glRotatef(m_yRotate, 0, 1, 0);
 
-    glCallList(m_nFlat);
+    gluLookAt(xCamera, yCamera, zCamera, xTimer, yTimer- flatSize, zTimer+ flatSize, 0, 1, 0);
 
+    createFlat();
     drawTrack();
-
-    drawLine(xTimer, yTimer, zTimer);
+    drawLine(xTimer, yTimer , zTimer);
 
 }
 
@@ -123,57 +77,29 @@ void Widget::mouseMoveEvent(QMouseEvent *pe)
     m_ptPosition = pe->pos();
 }
 
-GLuint Widget::createFlat( )
+void Widget::createFlat( )
 {
-    GLuint n = glGenLists(1);
+    glBegin(GL_QUADS);
+        qglColor(Qt::green);
 
-    glNewList(n, GL_COMPILE);
-        glBegin(GL_QUADS);
-            qglColor(Qt::green);
-            glVertex3f(-flatSize, -flatSize, flatSize);
-            glVertex3f(flatSize, -flatSize, flatSize);
-            glVertex3f(flatSize, -flatSize, -flatSize);
-            glVertex3f(-flatSize, -flatSize, -flatSize);
-
-        glEnd();
-    glEndList();
-    return n;
+        glVertex3f(-flatSize, -flatSize , flatSize);
+        glVertex3f(flatSize, -flatSize , flatSize );
+        glVertex3f(flatSize , -flatSize , -flatSize );
+        glVertex3f(-flatSize , -flatSize, -flatSize );
+    glEnd();
 }
 
-GLuint Widget::createCylinder(GLfloat fsize )
-{
-
-
-    double delta = 0.1;
-
-
-
-    GLuint n = glGenLists(1);
-    glNewList(n, GL_COMPILE);
-        glBegin(GL_LINES);
-         qglColor(Qt::red);
-         glVertex3f(-lineSize/2, -fsize + delta, fsize );
-         glVertex3f(lineSize/2, -fsize + delta, fsize);
-        glEnd();
-    glEndList();
-
-
-return n;
-}
 void Widget::drawLine(double x, double y, double z)
 {
     z -= lineSize;
 
-    //lines.push_back(Line(x-lineSize/2, - flatSize + 0.01,  z + flatSize, x+lineSize/2, - flatSize + 0.01 , z + flatSize));
-
     auto x1 = x;
-    auto z1 = z+ flatSize;
+    auto z1 = z + flatSize;
 
     auto x2 = x + lineSize*sin(alpha*3.14/180);
     auto z2 = z + lineSize*cos(alpha*3.14/180)+ flatSize;
 
     lines.push_back(Line(x1, - flatSize + 0.01,  z1, x2, - flatSize + 0.01 , z2));
-
 
     GLUquadricObj *quadObj;
     quadObj = gluNewQuadric();
@@ -181,51 +107,24 @@ void Widget::drawLine(double x, double y, double z)
     qglColor(Qt::red);
     gluQuadricDrawStyle(quadObj, GLU_FILL);
 
-
-    glPointSize(3);
-    glBegin(GL_POINTS);
-       glColor3d(1,1,0);
-       glVertex3d(x, - flatSize, z + flatSize); // первая точка
-
-    glEnd();
-
-    glTranslated(x, y - flatSize, z + flatSize);
+    glTranslated(x , y - flatSize , z + flatSize );
 
     glRotated(alpha, 0,1,0);
     glColor3d(1,0,0);
-    //glRotated(alpha, 0,1,0);
-    gluCylinder(quadObj, 0.05, 0.05, lineSize, 15, 15);
-
+    gluCylinder(quadObj, 0.01, 0.01, lineSize, 15, 15);
 
     gluDeleteQuadric(quadObj);
-
-
-
-
-    /*
-    lines.push_back(Line(x-lineSize/2, y - flatSize ,  z + flatSize, x+lineSize/2, y - flatSize , z + flatSize));
-
-    glLineWidth(2);
-    glBegin(GL_LINES);
-     qglColor(Qt::red);
-     glVertex3f(x-lineSize/2, y - flatSize + delta, z + flatSize );
-     glVertex3f(x+lineSize/2, y - flatSize + delta, z + flatSize );
-    glEnd();
-    */
 }
 
 void Widget::drawTrack()
 {
-    glLineWidth(3);
+    glLineWidth(5);
     glBegin(GL_LINES);
-     qglColor(Qt::blue);
+    qglColor(Qt::blue);
 
-     for(auto line:lines)
-     {
-        glVertex3f(line.x1, line.y1, line.z1);
-        glVertex3f(line.x2, line.y2, line.z2);
-     }
-   glEnd();
-
-
+    for(auto line:lines) {
+        glVertex3f(line.x1 , line.y1 , line.z1 );
+        glVertex3f(line.x2 , line.y2 , line.z2 );
+    }
+    glEnd();
 }
--- widget.h
+++ widget.h
@@ -8,9 +8,7 @@ struct Line
 {
     Line(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2):
         x1(x1), y1(y1), z1(z1), x2(x2), y2(y2), z2(z2)
-    {
-
-    }
+    {}
 
     GLfloat x1, y1, z1;
     GLfloat x2, y2, z2;
@@ -20,24 +18,26 @@ struct Line
 class Widget : public QGLWidget
 {
     Q_OBJECT
-private:
-    GLuint m_nFlat;
-    GLuint m_nCylinder;
-
+protected:
     GLfloat m_xRotate = 0;
     GLfloat m_yRotate = 0;
     QPoint m_ptPosition;
 
-    GLfloat flatSize = 1.2f;
+    GLfloat flatSize = 5.2f;
     GLfloat lineSize = 0.2f;
 
     GLfloat xTimer = 0;
     GLfloat yTimer = 0.05;
     GLfloat zTimer = 0;
-    double alpha = 0;
-    QTimer timer;
+    GLfloat alpha = 0;
 
+    QTimer timer;
     QList<Line> lines;
+
+    GLfloat xCamera = 0;
+    GLfloat yCamera = 0;
+    GLfloat zCamera = 0;
+
 
 protected:
     virtual void initializeGL() override;
@@ -46,9 +46,8 @@ protected:
     virtual void mousePressEvent(QMouseEvent *pe) override;
     virtual void mouseMoveEvent(QMouseEvent *pe) override;
 
-    GLuint createFlat();
+    void createFlat();
 
-    GLuint createCylinder(GLfloat fsize = 1.0f);
 
     void drawLine(double x, double y, double z);
     void drawTrack();
